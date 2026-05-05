ae70d0083c38a2959110cd0ab18ed2383e7d4aa9
fabio-sorrentino <fabio.sorrentino@edu-its.it>
2026-05-05 13:19:03 +0200

    creato-Button

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+function Button({ title, classes, handleClick }) {
+  return (
+    <button onClick={handleClick} className={`btn ${classes}`}>
+      {title}
+    </button>
+  )
+}
+
+export default Button;

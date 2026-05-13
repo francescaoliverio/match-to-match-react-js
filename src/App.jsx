ceb13f7cc01c69880ed80702d58639eac2bb4cdb
alessiamasuzzo <alessia.masuzzo@edu-its.it>
2026-05-13 16:02:58 +0200

    creata BottomNavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import PeopleGrid from "./components/PeopleGrid";
+// import PeopleGrid from "./components/PeopleGrid";
+import BottomNavBar from "./components/BottomNavBar";
 
 function App() {
   return (
     <>
       <h1>Match to Match</h1>
       <p>Ready, steady, go!</p>
-      <PeopleGrid />
+      {/* <PeopleGrid /> */}
+      <BottomNavBar currentPage='explore'></BottomNavBar>
     </>
   );
 }
--- src/components/BottomNavBar.jsx
+++ src/components/BottomNavBar.jsx
@@ -1,13 +1,35 @@
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+// 🧭 explore
+// -- outlined
+import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
+// -- filled
+import ExploreIcon from "@mui/icons-material/Explore";
+
+// 🤝 match
+// -- outlined
+import HandshakeOutlinedIcon from "@mui/icons-material/HandshakeOutlined";
+// -- filled
+import HandshakeIcon from "@mui/icons-material/Handshake";
+
+// 💬 chat
+// -- outlined
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+// -- filled
+import ChatBubbleOutlinedIcon from "@mui/icons-material/ChatBubbleOutlined";
+
+// 👤 profile
+// -- outlined
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+// -- filled
+import PersonIcon from "@mui/icons-material/Person";
 
 export default function BottomNavBar({currentPage}){
     return(
         <>
         <nav>
-            <a href="#explore"></a>
-            <a href="#match"></a>
-            <a href="#chat"></a>
-            <a href="#profile"><PersonOutlineOutlined/></a>
+            <a href="#explore">{(currentPage === "explore" ? <ExploreIcon/> : <ExpoloreOutlinedIcon/>)}</a>
+            <a href="#match">{(currentPage === "match" ? <HandshakeIcon/> : <HandshakeOutlinedIcon/>)}</a>
+            <a href="#chat">{(currentPage === "chat" ? <ChatBubbleOutlinedIcon/> : <ChatBubbleOutlineOutlinedIcon/>)}</a>
+            <a href="#profile">{(currentPage === "profile" ? <PersonIcon/> : <PersonOutlinedIcon/>)}</a>
         </nav>
         </>
     )

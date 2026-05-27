7e079fac4ca24a98aceed1e912098f884e2f1ece
fabio-sorrentino <fabio.sorrentino@edu-its.it>
2026-05-27 16:20:02 +0200

    fix(searchbar): refactor, add children

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 
 export default function SearchBar({placeholder, children}) {
-  const[input, setNewInput] = useState("");
+  const[input, setInput] = useState("");
 
-  function handleChangeInput(t) {
-    setNewInput(t.target.value);
+  function handleChangeInput(i) {
+    setInput(i.target.value);
   }
 
   return (
-    <>
+    <div>
       <input type="text" value={input} onChange={handleChangeInput} placeholder={placeholder} />
-    </>
+      {children}
+    </div>
   );
 }

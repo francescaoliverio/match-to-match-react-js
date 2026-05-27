f706f1cf068618c8675881934f2a0157c99ad7c2
alessiamasuzzo <alessia.masuzzo@edu-its.it>
2026-05-27 17:59:25 +0200

    fix(match): fix fetch from API and refactor

--- src/pages/Match.jsx
+++ src/pages/Match.jsx
@@ -1,20 +1,16 @@
 import { useEffect, useState } from "react";
 
 export default function Match() {
-
-    const [data, setData] = useState(null)
     const [users, setUsers] = useState(null)
 
 useEffect(() => {
   const fetchData = async () => {
     try {
-      const response = await fetch('https://mock-api-server-production-7f5d.up.railway.app/match-to-match/api')
+      const response = await fetch('/api/users')
       console.log('Status:', response.status)
-      const data = await response.json()
-      console.log('Data:', data)
-      console.log('Users:', data.users)
-
-      setUsers(data.users)
+      const users = await response.json()
+      console.log('Users:', users)
+      setUsers(users)
     } catch (error) {
       console.error('Errore nel fetch dei dati:', error)
     }
@@ -26,11 +22,13 @@ useEffect(() => {
   return (
     <>
       <h1>Match</h1>
-       <h2>Prova fetch</h2>
        {users ? (
         <ul>
           {users.map((user) => (
-            <li key={user.id}>{user.firstName}</li>
+            <li key={user.id}>
+              <h5>{user.firstName}</h5>
+              <p>{user.bio}</p>
+            </li>
           ))}
         </ul>
        ) :(

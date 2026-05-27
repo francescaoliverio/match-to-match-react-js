944cbc0ed005ce596f87843998aaf33e5f5bacda
Francesca <francesca.oliverio@edu-its.it>
2026-05-27 22:56:19 +0200

    feat(chat): add searchbar

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,7 +1,16 @@
+import SearchBar from "../components/SearchBar";
+import Button from "../components/Button"
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
 export default function Chat() {
   return (
     <>
       <h1>Chat</h1>
+      <SearchBar placeholder="search">
+        <Button variant="transparent" className="p-1.5">
+          <SearchOutlinedIcon />
+        </Button>
+      </SearchBar>
     </>
   );
 }

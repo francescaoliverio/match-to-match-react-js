f604f001e4dfb4434efde0794dabffc6e8f43d37
Francesca <francesca.oliverio@edu-its.it>
2026-05-27 22:51:13 +0200

    feat(button): add variant transparent

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,13 @@
 import { cn } from "../lib/utils";
 
-// import "../styles/Button.css";
-
-const BASE_STYLES = "flex items-center justify-center gap-2.5 px-5 py-2.5 rounded-full w-fit font-bold cursor-pointer cursor-pointer disabled:cursor-default";
+const BASE_STYLES = "flex items-center justify-center gap-2.5 px-5 py-2.5 rounded-full w-fit font-bold cursor-pointer disabled:cursor-default";
 
 const VARIANT_STYLES = {
   primary: "text-light bg-primary hover:bg-primary-hover disabled:bg-primary-disabled",
   secondary: "text-ink bg-secondary hover:bg-secondary-hover disabled:bg-secondary-disabled",
   tertiary: "text-light bg-tertiary hover:bg-tertiary-hover disabled:bg-tertiary-disabled",
   light: "text-tertiary bg-light hover:bg-light-hover disabled:bg-light-disabled",
+  transparent: "text-ink hover:text-tertiary disabled:text-grey-light bg-transparent",
 };
 
 export default function Button({ label, variant="light", className, handleClick, children, ...props }) {

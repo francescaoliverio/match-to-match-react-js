768b2c7313f8039082f20b36547d9422508faf7e
Francesca <francesca.oliverio@edu-its.it>
2026-05-28 22:23:06 +0200

    feat(matchsearch): add for to label

--- src/components/MatchSearch.jsx
+++ src/components/MatchSearch.jsx
@@ -7,8 +7,6 @@ import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined';
 import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
-import { cn } from "../lib/utils";
-
 export default function MatchSearch() {
   const [active, setActive] = useState("what")
 
@@ -17,16 +15,16 @@ export default function MatchSearch() {
   }
 
   return (
-    <div className="matchsearch bg-transparent-tertiary flex items-center justify-center p-5 gap-5 rounded-full w-fit">
+    <div className="bg-transparent-tertiary flex items-center justify-center p-5 gap-5 rounded-full w-fit">
       <div onClick={() => handleClick("what")}>
         {active === "what" ? (
           <SearchBar id="what" placeholder="what..." className="flex-2 flex-row-reverse p-2.5 gap-2.5">
-            <label className="text-tertiary">
+            <label htmlFor="what" className="text-tertiary">
               <CategoryOutlinedIcon color="inherit" />
             </label>
           </SearchBar>
         ) : (
-          <label className="text-transparent-white">
+          <label htmlFor="what" className="text-transparent-white">
             <CategoryOutlinedIcon color="inherit" />
           </label>
         )}
@@ -34,12 +32,12 @@ export default function MatchSearch() {
       <div onClick={() => handleClick("where")}>
         {active === "where" ? (
           <SearchBar id="where" placeholder="where..." className="flex-2 flex-row-reverse p-2.5 gap-2.5">
-            <label className="text-tertiary">
+            <label htmlFor="where" className="text-tertiary">
               <PlaceOutlinedIcon color="inherit" />
             </label>
           </SearchBar>
         ) : (
-          <label className="text-transparent-white">
+          <label htmlFor="where" className="text-transparent-white">
             <PlaceOutlinedIcon color="inherit" />
           </label>
         )}
@@ -47,12 +45,12 @@ export default function MatchSearch() {
       <div onClick={() => handleClick("when")}>
         {active === "when" ? (
           <SearchBar id="when" placeholder="when..." className="flex-2 flex-row-reverse p-2.5 gap-2.5">
-            <label className="text-tertiary">
+            <label htmlFor="when" className="text-tertiary">
               <CalendarTodayOutlinedIcon color="inherit" />
             </label>
           </SearchBar>
         ) : (
-          <label className="text-transparent-white">
+          <label htmlFor="when" className="text-transparent-white">
             <CalendarTodayOutlinedIcon color="inherit" />
           </label>
         )}

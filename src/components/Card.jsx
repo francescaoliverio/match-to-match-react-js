ef6de4905e46570adc190fe55733a6a33886a5f7
Francesca <francesca.oliverio@edu-its.it>
2026-05-30 12:34:04 +0200

    feat(card): add box shadow, scale when :hover

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,18 +2,20 @@
 
 import { cn } from "../lib/utils";
 
-const BASE_STYLES = "flex flex-col justify-stretch rounded-2xl w-2xs bg-white overflow-hidden cursor-pointer disabled:cursor-default";
+const BASE_STYLES = "flex flex-col justify-stretch rounded-2xl w-full bg-white shadow-md shadow-dark-overlay overflow-hidden cursor-pointer disabled:cursor-default";
 
 const VARIANT_STYLES = {
-  border: "border border-grey-light"
+  none: "",
+  border: "border border-grey-light",
+  hoverScale: "hover:scale-105 transition-transform motion-reduce:transition-none motion-reduce:hover:transform-none",
 };
 
-export default function Card({ person, className, variant = "border", ...props }) {
+export default function Card({ person, className, variant = "none", ...props }) {
   return (
     <div className={cn(BASE_STYLES, VARIANT_STYLES[variant], className)} {...props}>
       <img src={person.avatar} alt={person.firstName} />
       <div className="p-2.5">
-        <strong>{person.firstName}</strong>
+        <strong className="text-lg">{person.firstName}</strong>
         <p className="line-clamp-3">{person.bio}</p>
       </div>
     </div>

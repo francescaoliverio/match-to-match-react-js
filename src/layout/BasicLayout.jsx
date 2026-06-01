1744d5f30ab8eec94415f3c9ea4a2079f2e6b239
Francesca <francesca.oliverio@edu-its.it>
2026-06-01 12:18:32 +0200

    feat(layout): add dynamic background image based on current route, with useLocation

--- src/layout/BasicLayout.jsx
+++ src/layout/BasicLayout.jsx
@@ -1,13 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import BottomNavBar from "../components/BottomNavBar";
 
+const backgrounds = {
+  "/": "url('/images/background/selfie-03.webp')",
+  "/match": "url('/images/background/friends-02.webp')",
+  "/chat":  "url('/images/background/friends-01.webp')",
+  "/profile": "url('/images/background/selfie-02.jpg')",
+}
+const bgPaths = ["/match", "/chat", "/profile"]
+
 export default function BasicLayout() {
+  const { pathname } = useLocation()
+  const bgKey = bgPaths.find(key => pathname.startsWith(key)) ?? "/"
+
   return (
-    <>
-      <main  className="flex flex-col grow items-center justify-start overflow-scroll">
-        <Outlet/>
+    <div className="h-screen flex flex-col bg-cover bg-center" style={{ backgroundImage: backgrounds[bgKey] }}>
+      <main className="flex flex-col grow items-center justify-start overflow-scroll  bg-dark-overlay">
+        <Outlet />
       </main>
-        <BottomNavBar />
-    </>
-  );
+      <BottomNavBar />
+    </div>
+  )
 }

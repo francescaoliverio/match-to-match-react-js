bcd297e342e1f66bcab8eaa50fb3e66c254307af
gaia-giachero <gaia.gicahero@edu-its.it>
2026-06-01 18:56:13 +0200

    feat(explore): add title, categoriesGrid, usersGrid with text

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,13 +1,29 @@
 import MatchSearch from "../components/MatchSearch";
 import UsersGrid from "../components/UsersGrid";
+import CategoriesGrid from "../components/CategoriesGrid";
 
 export default function Explore() {
   return (
     <>
-      <h1>Explore</h1>
-      <MatchSearch />
+      <div>
+        <h1>MATCH TO MATCH</h1>
+        <MatchSearch />
+      </div>
+      <div>
+        <h3>A cosa vorresti giocare?</h3>
+        <p>Esplora le categorie</p>
+        <CategoriesGrid />
+      </div>
+      <div>
+        <h2>THE RIGHT MATCH IS WAITING TO MEET YOU!</h2>
+      </div>
       <div className="p-5 self-stretch">
-        <UsersGrid limit={4}/>
+        <h3>Ready to Match?</h3>
+        <p>Proposte di match per te</p>
+        <UsersGrid limit={10} />
+      </div>
+      <div>
+        <h3>Explore the map</h3>
       </div>
     </>
   );

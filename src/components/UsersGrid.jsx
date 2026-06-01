5834524c97a64c4ffbcc741356c25ac5c241a6ff
Francesca <francesca.oliverio@edu-its.it>
2026-06-01 13:52:56 +0200

    feat(usersgrid): add CardSkeleton while loading, improve error displaying

--- src/components/UsersGrid.jsx
+++ src/components/UsersGrid.jsx
@@ -1,26 +1,34 @@
 // src/components/UsersGrid.jsx
 
 import { useUsers } from "../hooks/useUsers"
-import Card from "./Card"
+import Card, {CardSkeleton} from "./Card"
+
+const gridTemplate = {
+  gridTemplateColumns: `repeat(auto-fill, minmax(8rem, 12rem))`,
+  gridTemplateRows: `repeat(auto, auto)`,
+}
 
 export default function UsersGrid({limit}) {
-  const gridTemplate = {
-    gridTemplateColumns: `repeat(auto-fill, minmax(8rem, 12rem))`,
-    gridTemplateRows: `repeat(auto, auto)`,
-  }
-
+  // store return values from useUsers()
   const { users, loading, error } = useUsers()
-  // prevent rendering users.map before promise is resolved
-  if (loading) return <p>Caricamento...</p>
-  if (error) return <p>Errore: {error}</p>
-  // only when loading completed without errors: map
-  const visible = limit ? users.slice(1, limit+1) : users
+  // if error, return error
+  if (error) return (
+    <div className="flex flex-col items-center gap-5 p-5 bg-primary text-white rounded-2xl shadow-md shadow-dark-overlay">
+      <p>Error: {error}</p>
+    </div>
+    )
+
+  // if prop limit is provided, render [limit] cards
+  const skeletonCount = limit ?? 6
+  // skeleton while loading: prevent rendering users.map before promise is resolved
   return (
     <div className="grid gap-8 justify-center-safe" style={gridTemplate}>
-      {visible.map((user) => (
-        <Card key={user.id} person={user} variant="hoverScale">
-        </Card>
-      ))}
+      {loading
+        ? Array.from({ length: skeletonCount }).map((_, i) => <CardSkeleton key={i} />)
+        : users.slice(1, limit ? limit + 1 : undefined).map((user) => 
+          <Card key={user.id} person={user} variant="hoverScale" />
+        )
+      }
     </div>
   )
 }

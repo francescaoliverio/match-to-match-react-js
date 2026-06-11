6afd9883082b9510077aa542a2e027e5acc0790d
Francesca Oliverio <francesca.oliverio@edu-its.it>
2026-06-11 13:10:44 +0200

    feat(profile): use auth context to render login btn / logout btn + profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,30 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import Button from "../components/Button";
+import { useAuth } from "../context/AuthContext";
 
 export default function Profile() {
+  const { logout } = useAuth();
+  const { user } = useAuth();
+
   return (
-    <>
-      <h1>Profile</h1>
-      <Link to="/profile/login"><Button label="Accedi" variant="primary"/></Link>
-    </>
+    <div className="p-5 gap-2.5 flex flex-col items-center justify-start">
+      {user ? (
+        <>
+          {/* Profile Picture */}
+          <div className="w-20 h-20 rounded-full overflow-hidden flex justify-center items-center bg-tertiary">
+            <img src={user.avatar} alt="profile picture" />
+          </div>
+          {/* User Name */}
+          <h2>
+            {user.firstName} {user.lastName}
+          </h2>
+          <Button label="Esci" variant="primary" onClick={logout} />
+        </>
+      ) : (
+        <Link to="/profile/login">
+          <Button label="Accedi" variant="primary" />
+        </Link>
+      )}
+    </div>
   );
 }

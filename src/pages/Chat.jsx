bd24bc8e5a994f1c93e91ef0aabac417ac6efa81
fabio-sorrentino <fabio.sorrentino@edu-its.it>
2026-06-12 19:58:37 +0200

    style(chat) update chat styles

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -6,7 +6,7 @@ export default function Chat() {
   return (
     <>
       <h1>Chat</h1>
-      <div className="grow flex flex-row items-stretch justify-center gap-5 w-full p-5">
+      <div className="grow flex flex-col md:flex-row items-stretch justify-center gap-5 w-full p-5">
         <ChatList />
         <OpenChat />
       </div>

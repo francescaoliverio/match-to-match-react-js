edbdba23defbc4f9ced4516a12571d794f807366
Francesca Oliverio <francesca.oliverio@edu-its.it>
2026-06-12 16:43:46 +0200

    feat(profile): add profile infos (pic, name, bio etc.)

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,30 +1,35 @@
-import { Link } from "react-router-dom";
-import Button from "../components/Button";
 import { useAuth } from "../context/AuthContext";
 
+// 📍 location
+import PlaceOutlinedIcon from "@mui/icons-material/PlaceOutlined";
+// 📆 calendar
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+
 export default function Profile() {
-  const { logout } = useAuth();
   const { user } = useAuth();
 
   return (
     <div className="p-5 gap-2.5 flex flex-col items-center justify-start">
-      {user ? (
-        <>
-          {/* Profile Picture */}
-          <div className="w-20 h-20 rounded-full overflow-hidden flex justify-center items-center bg-tertiary">
-            <img src={user.avatar} alt="profile picture" />
-          </div>
-          {/* User Name */}
-          <h2>
-            {user.firstName} {user.lastName}
-          </h2>
-          <Button label="Esci" variant="primary" onClick={logout} />
-        </>
-      ) : (
-        <Link to="/profile/login">
-          <Button label="Accedi" variant="primary" />
-        </Link>
-      )}
+      <div className="w-xs flex flex-col items-center gap-5 p-5 bg-transparent-white text-ink rounded-2xl shadow-md shadow-dark-overlay">
+        {/* Profile Picture */}
+        <div className="w-20 h-20 rounded-full overflow-hidden flex justify-center items-center bg-tertiary">
+          <img src={user.avatar} alt="profile picture" />
+        </div>
+        {/* User Name */}
+        <h3>
+          {user.firstName} {user.lastName}
+        </h3>
+        <div className="flex flex-row justify-evenly w-full">
+          <p>
+            <PlaceOutlinedIcon />
+            {user.city}
+          </p>
+          <p>
+            <CalendarTodayOutlinedIcon /> {user.age} anni
+          </p>
+        </div>
+        <p>{user.bio}</p>
+      </div>
     </div>
   );
 }

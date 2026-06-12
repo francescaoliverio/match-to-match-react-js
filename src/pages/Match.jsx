bc7e8104ad6407932be14a7e5a4d048da573f9d3
Francesca Oliverio <francesca.oliverio@edu-its.it>
2026-06-13 00:33:56 +0200

    fix(match): change title color + text shadow to improve legibility

--- src/pages/Match.jsx
+++ src/pages/Match.jsx
@@ -4,7 +4,7 @@ export default function Match() {
 
   return (
     <>
-      <h1>Ready to Match?</h1>
+      <h1 className="text-primary text-shadow-lg shadow-dark-overlay">Ready to Match?</h1>
       <div className="p-5 self-stretch">
         <UsersGrid cols={3} rows={2} pending={true}/>
       </div>

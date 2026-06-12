5e5edc368c424c094e6537f1db2ba84df37a038a
Francesca Oliverio <francesca.oliverio@edu-its.it>
2026-06-12 16:49:49 +0200

    feat(protected paths): only explore and login visible if not logged

--- src/layout/BasicLayout.jsx
+++ src/layout/BasicLayout.jsx
@@ -7,7 +7,7 @@ import Button from "../components/Button";
 const backgrounds = {
   "/": "url('/images/background/friends.jpg')",
   "/match": "url('/images/background/friends-02.webp')",
-  "/chat":  "url('/images/background/friends-01.webp')",
+  "/chat": "url('/images/background/friends-01.webp')",
   "/profile": "url('/images/background/selfie-02.jpg')",
 }
 const bgPaths = ["/match", "/chat", "/profile"]
@@ -19,12 +19,22 @@ export default function BasicLayout() {
   // Page background depending on location
   const { pathname } = useLocation();
   const bgKey = bgPaths.find((key) => pathname.startsWith(key)) ?? "/";
+  // Protected paths: need to log in to access the content of match, chat, profile
+  const protectedPaths = ["/match", "/chat", "/profile"];
+  const publicPaths = ["/profile/login"];
+  const needLogin = !publicPaths.includes(pathname) && protectedPaths.some((path) => pathname.startsWith(path));
 
   return (
     <div className="h-screen flex flex-col bg-cover bg-top" style={{ backgroundImage: backgrounds[bgKey] }}>
       <TopNavBar />
       <main className="flex flex-col grow items-center justify-start overflow-auto bg-light-overlay backdrop-saturate-120 backdrop-blur-[2px]">
-        <Outlet />
+        {(user || !needLogin) ? (
+          <Outlet />
+        ) : (
+          <Link to="/profile/login">
+            <Button label="Accedi" variant="primary" />
+          </Link>
+        )}
       </main>
       <BottomNavBar />
     </div>

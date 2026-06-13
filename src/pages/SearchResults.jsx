82eee67f8323077a476769004de574be8a037efa
Francesca Oliverio <francesca.oliverio@edu-its.it>
2026-06-13 19:03:03 +0200

    feat(SearchResults): change loading and error output for consistency
    - loading shows card skeletons
    - errors show error box

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,36 +1,39 @@
 //src/pages/SearchResults.jsx
 
-import { useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { useCategories } from "../hooks/useCategories";
-import { useUsers } from "../hooks/useUsers";
-import Card from "../components/Card";
-
-import { ERROR_BOX_STYLES, WARNING_BOX_STYLES } from "../styles/styles";
+import { useLocation } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { useCategories } from "../hooks/useCategories"
+import { useUsers } from "../hooks/useUsers"
+import Card, { CardSkeleton } from "../components/Card"
+import ErrorBox from "../components/ErrorBox"
+import { GRID_STYLES, SECTION_STYLES } from "../styles/styles"
 
 export default function SearchResults() {
-  const location = useLocation();
-  const query = new URLSearchParams(location.search);
-  const term = query.get("q") || "";
+  const location = useLocation()
+  const query = new URLSearchParams(location.search)
+  const term = query.get("q") || ""
 
-  const { users, loading: userLoading, error: userError } = useUsers();
-  const { categories, loading: catLoading, error: catError } = useCategories();
+  const { categories, loading: catLoading, error: catError } = useCategories()
+  const { users, loading: userLoading, error: userError } = useUsers()
 
-  if (userLoading || catLoading) {
-    return <div className={WARNING_BOX_STYLES}>Caricamento...</div>;
-  }
+  // If error, return ErrorBox
+  if (catError || userError) return (
+    <ErrorBox><strong>Errore: </strong>{catError || userError || "Errore nel caricamento"}</ErrorBox>
+  )
 
-  if (userError || catError) {
-    return <div className={ERROR_BOX_STYLES}>Errore nel caricamento</div>;
-  }
+  // Check if term matches category
+  const matchedCategory = categories?.find((cat) => cat.name.trim().toLowerCase() === term.trim().toLowerCase())
 
-  const matchedCategory = categories?.find((cat) => cat.name.trim().toLowerCase() === term.trim().toLowerCase());
+  // If not an existing category, return ErrorBox
+  if (!catLoading && !matchedCategory) return (
+    <ErrorBox>
+      <h2 className="-mt-2.5">Che peccato!</h2>
+      <p>Siamo spiacenti, ma la categoria <strong>{term}</strong> non esiste.</p>
+    </ErrorBox>
+  )
 
-  if (!matchedCategory) {
-    return <div className={WARNING_BOX_STYLES}>Categoria inesistente</div>;
-  }
-
-  const filtered = users?.filter((user) => user.activities.includes(matchedCategory.id));
+  // Filter users that match category
+  const filtered = users?.filter((user) => user.activities.includes(matchedCategory?.id))
 
   return (
     <section className={SECTION_STYLES}>
@@ -38,7 +41,9 @@ export default function SearchResults() {
       <h1 className="text-center">{catLoading ? "Caricamento..." : `Categoria: ${matchedCategory?.name}`}</h1>
       {/* Cards: show skeletons until users are fetched and filtered */}
       <div className={GRID_STYLES} style={{ gridTemplateColumns: `repeat(auto-fit, 12rem)` }}>
-        {userLoading || catLoading ? Array.from({ length: 5 }).map((_, i) => <CardSkeleton key={i} />) : filtered?.map((user) => <Card key={user.id} obj={user} img={user.avatar} title={user.firstName} description={user.bio} variant="hoverScale" />)}
+        {userLoading || catLoading
+          ? Array.from({ length: 5 }).map((_, i) => <CardSkeleton key={i} />)
+          : filtered?.map((user) => <Card key={user.id} obj={user} img={user.avatar} title={user.firstName} description={user.bio} variant="hoverScale" />)}
       </div>
     </section>
   )

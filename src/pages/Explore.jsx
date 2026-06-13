81365c9bf0228b27038fc16667943abe84dbc26f
Francesca Oliverio <francesca.oliverio@edu-its.it>
2026-06-13 19:26:02 +0200

    feat(explore): implement working carousel for categories

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,15 +1,28 @@
 // src/pages/Explore.jsx
 
-import MatchSearch from "../components/MatchSearch";
-import UsersGrid from "../components/UsersGrid";
-import CategoriesGrid from "../components/CategoriesGrid";
-import Button from "../components/Button";
-import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined";
-import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
-
-import { SECTION_STYLES } from "../styles/styles";
+import { useRef } from "react"
+import MatchSearch from "../components/MatchSearch"
+import UsersGrid from "../components/UsersGrid"
+import CategoriesGrid from "../components/CategoriesGrid"
+import Button from "../components/Button"
+import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined"
+import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined"
+import { SECTION_STYLES } from "../styles/styles"
 
 export default function Explore() {
+  // for categories carousel
+  const carouselRef = useRef(null)
+  const scroll = (direction) => {
+    if (carouselRef.current) {
+      // 224px = card width (12rem = 192px) + gap (2rem = 32px)
+      const scrollAmount = direction === "left" ? -224 : 224
+      carouselRef.current.scrollBy({
+        left: scrollAmount,
+        behavior: "smooth",
+      })
+    }
+  }
+
   return (
     <>
       {/* Hero */}
@@ -22,13 +35,13 @@ export default function Explore() {
         <h3 className="text-center">Scegli la prossima sfida</h3>
         <p>Esplora le categorie</p>
         <div className="w-full flex flex-row justify-center items-center gap-5">
-          <Button className="p-2.5">
+          <Button onClick={() => scroll("left")} className="p-2.5">
             <ArrowBackIosOutlinedIcon />
           </Button>
-          <div className="p-2.5 overflow-x-visible overflow-y-auto">
-            <CategoriesGrid className="w-fit"/>
+          <div ref={carouselRef} className="p-2.5 overflow-y-visible overflow-x-auto scrollbar-none [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none]">
+            <CategoriesGrid className="w-fit" />
           </div>
-          <Button className="p-2.5">
+          <Button onClick={() => scroll("right")} className="p-2.5">
             <ArrowForwardIosOutlinedIcon />
           </Button>
         </div>
@@ -47,5 +60,5 @@ export default function Explore() {
         </figure>
       </section>
     </>
-  );
+  )
 }

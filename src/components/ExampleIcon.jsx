abe5ec6896d43cd6560bb5390c32af32cb540b76
Francesca Oliverio <francesca.oliverio@edu-its.it>
2026-06-17 22:14:35 +0200

    chore(exampleicon): delete unused example component

--- src/components/ExampleIcon.jsx
+++
@@ -1,9 +0,0 @@
-import AddOutlinedIcon from '@mui/icons-material/AddOutlined'
-
-export default function Icon() {
-  return (
-    <>
-      <AddOutlinedIcon color='#000' fontSize='large' />
-    </>
-  )
-}
